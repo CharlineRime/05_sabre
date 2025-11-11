11afa01ac141f9800e5add7e8317bab3a3b50009
Charline Rime <160761068+CharlineRime@users.noreply.github.com>
2025-11-11 14:50:55 +0100

    Start javascript for some sections

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,37 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+// Page de titre
+
+gsap.to("h1", {
+  opacity: 1,
+  y: -150,
+  scrollTrigger: {
+    trigger: ".first-section-title",
+    scrub: true,
+  },
+});
+
+// Wrap
+const timeline1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-section",
+    scrub: true,
+    pin: true,
+  },
+});
+
+timeline1.to("#object-text", {
+  opacity: 1,
+  y: "25%",
+});
+timeline1.to("#object-left-1", {
+  opacity: 1,
+  x: "300%",
+});
+
+timeline1.to("#object-left-2", {
+  opacity: 1,
+  x: "250%",
+});
