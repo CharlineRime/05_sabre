88c4be16ea796ed8fd1670074a9d1d0f2f598a54
Charline Rime <160761068+CharlineRime@users.noreply.github.com>
2025-11-12 08:46:29 +0100

    eigth and tenth section javascript

--- src/js/main.js
+++ src/js/main.js
@@ -78,3 +78,44 @@ timeline3.to("#sixth-section-left-1", {
   opacity: 1,
   x: "200%",
 });
+
+// Wrap 4
+const timeline4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".eigth-section",
+    scrub: true,
+    pin: true,
+  },
+});
+
+timeline4.to("#eigth-section-text", {
+  opacity: 1,
+  y: "25%",
+});
+timeline4.to("#eigth-section-left-1", {
+  opacity: 1,
+  x: "-150%",
+});
+
+timeline4.to("#eigth-section-left-2", {
+  opacity: 1,
+  x: "-150%",
+});
+
+// Wrap 5
+const timeline5 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".tenth-section",
+    scrub: true,
+    pin: true,
+  },
+});
+
+timeline5.to("#tenth-section-text", {
+  opacity: 1,
+  y: "25%",
+});
+timeline5.to("#tenth-section", {
+  opacity: 1,
+  x: "125%",
+});
